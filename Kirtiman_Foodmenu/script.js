d27321dfc71332d67d5ca61f1883bc818fc97528
Kirtiman-Sigmainfo <kirtiman.dutta@sigmainfo.net>
2020-10-12 16:08:18 +0530

    First Commit

---
+++ Kirtiman_Foodmenu/script.js
@@ -0,0 +1,18 @@
+var age=prompt("Enter your age");
+var year=2020-age;
+if(year<=2020 && year>=2000)
+{
+    location.replace("twenty.html")
+}
+else if(year<2000 && year>=1975)
+{
+    location.replace("fortees.html");
+}
+else if(year<1975 && year>=1960)
+{
+    location.replace("sixtees/index.html");
+}
+else
+{
+
+}
